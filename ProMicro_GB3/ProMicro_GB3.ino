d8098d7ee74d298282fb2b7296c9dfce18aefe8c
vascofazza <fscozzafava@imacdifederico.lan>
2018-01-21 23:48:46 +0100

    refactoring

--- ProMicro_GB3/ProMicro_GB3.ino
+++ ProMicro_GB3/ProMicro_GB3.ino
@@ -1,25 +1,29 @@
 /*
-* 
-* This sketch handles button keys emulation using direct port manipulation, battery measurement and screen brightness control.
-* This file originates from vascofazza's GameBoy MK3 raspberry project.
-* Author: Federico Scozzafava
-* 
-* THIS HEADER MUST REMAIN WITH THIS FILE AT ALL TIMES
-*
-* This firmware is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This firmware is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this repo. If not, see <http://www.gnu.org/licenses/>.
-*
+
+  This sketch handles button keys emulation using direct port manipulation, battery measurement and screen brightness control.
+  This file originates from vascofazza's GameBoy MK3 raspberry project.
+  Author: Federico Scozzafava
+
+  THIS HEADER MUST REMAIN WITH THIS FILE AT ALL TIMES
+
+  This firmware is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This firmware is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this repo. If not, see <http://www.gnu.org/licenses/>.
+
 */
+//imports
+#include <Keyboard.h>
+#include <EEPROM.h>
+
 #define KEY_LEFT_CTRL  0x80
 #define KEY_LEFT_SHIFT 0x81
 #define KEY_LEFT_ALT   0x82
@@ -28,7 +32,6 @@
 #define KEY_RIGHT_SHIFT    0x85
 #define KEY_RIGHT_ALT  0x86
 #define KEY_RIGHT_GUI  0x87
-
 #define KEY_UP_ARROW   0xDA
 #define KEY_DOWN_ARROW 0xD9
 #define KEY_LEFT_ARROW 0xD8
@@ -56,33 +59,34 @@
 #define KEY_F10        0xCB
 #define KEY_F11        0xCC
 #define KEY_F12        0xCD
-
-#include <Keyboard.h>
-#include <EEPROM.h>
-
+//
 #define BatteryLevelPin A0
 #define BrightnessAddress 0x00
 #define OFFSET 20
-
 #define WINDOW 200
-int analogread[WINDOW];
-long total;
-int readIndex = 0;
+//
+unsigned int analogread[WINDOW];
+unsigned long total;
+unsigned int readIndex = 0;
+byte predStatusB = B00000000;
+byte predStatusD = B00000000;
+byte predStatusE = B00000000;
+byte predStatusF = B00000000;
+byte portB, portD, portE, portF;
+bool MOD, UP, DOWN, LEFT, RIGHT, L, R, A, B, X, Y, START, SELECT;
+byte counter;
+byte level = 0;
+bool keyPressed[13];
 
 // buttons A B X Y L R U D L R ST SEL MOD
-static char keys[] = {KEY_LEFT_CTRL, KEY_LEFT_ALT, 'z', 'x', KEY_LEFT_SHIFT, KEY_RIGHT_SHIFT, KEY_UP_ARROW, KEY_DOWN_ARROW,
-                      KEY_LEFT_ARROW, KEY_RIGHT_ARROW, KEY_RETURN, ' '
-                     };
-static bool keyPressed[13];
-static byte level = 0;
+const char keys[] = {KEY_LEFT_CTRL, KEY_LEFT_ALT, 'z', 'x', KEY_LEFT_SHIFT, KEY_RIGHT_SHIFT, KEY_UP_ARROW, KEY_DOWN_ARROW,
+                     KEY_LEFT_ARROW, KEY_RIGHT_ARROW, KEY_RETURN, ' '
+                    };
+
 void setup() {
   //init battery readings
   for (int i = 0; i < WINDOW; i++)
     analogread[i] = 0;
-  // open the serial port:
-  Serial.begin(9600);
-  // initialize control over the keyboard:
-  Keyboard.begin();
   DDRE &= B10111111; // MOD
   PORTE |= ~B10111111;
   //      TXLD TR
@@ -104,28 +108,36 @@ void setup() {
     EEPROM.write(BrightnessAddress, 1);
     level = 1;
   }
-  delay(1000);
   PORTB = (PORTB & 0xF1) | ((7 - (level & 7)) << 1);
+  // open the serial port:
+  Serial.begin(9600);
+  delay(500);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+  delay(500);
 }
 void loop() {
 
-  // X - B - A
-  auto portB = PINB;
-  auto portF = PINF;
-  auto portD = PIND;
-  auto portE = PINE;
+  portB = PINB;
+  portF = PINF;
+  portD = PIND;
+  portE = PINE;
 
-  //battery read, using 1ms from debouncing
+  //battery reading update
   readBattery();
 
+  //processing serial
+  processSerial();
+
   //debouncing
-  _delay_ms(4);
+  _delay_ms(5);
 
-  int counter = 0;
+  counter = 0;
 
-  if (portB == PINB)
+  if (portB == PINB && portB != predStatusB)
   {
-    bool A = portB & B00010000;
+    predStatusB = portB;
+    A = portB & B00010000;
     if (A != keyPressed[counter])
     {
       keyPressed[counter] = A;
@@ -135,7 +147,7 @@ void loop() {
         Keyboard.press(keys[counter]);
     }
     counter++;
-    bool B = portB & B00100000;
+    B = portB & B00100000;
     if (B != keyPressed[counter])
     {
       keyPressed[counter] = B;
@@ -145,7 +157,7 @@ void loop() {
         Keyboard.press(keys[counter]);
     }
     counter++;
-    bool X = portB & B01000000;
+    X = portB & B01000000;
     if (X != keyPressed[counter])
     {
       keyPressed[counter] = X;
@@ -158,10 +170,11 @@ void loop() {
   else
     counter += 2;
 
-  if (portF == PINF)
+  if (portF == PINF  && portF != predStatusF)
   {
+    predStatusF = portF;
     counter++;
-    bool Y = portF & B00010000;
+    Y = portF & B00010000;
     if (Y != keyPressed[counter])
     {
       keyPressed[counter] = Y;
@@ -171,21 +184,21 @@ void loop() {
         Keyboard.press(keys[counter]);
     }
     counter++;
-    bool LL = portF & B00100000;
-    if (LL != keyPressed[counter])
+    L = portF & B00100000;
+    if (L != keyPressed[counter])
     {
-      keyPressed[counter] = LL;
-      if (LL)
+      keyPressed[counter] = L;
+      if (L)
         Keyboard.release(keys[counter]);
       else
         Keyboard.press(keys[counter]);
     }
     counter++;
-    bool RR = portF & B01000000;
-    if (RR != keyPressed[counter])
+    R = portF & B01000000;
+    if (R != keyPressed[counter])
     {
-      keyPressed[counter] = RR;
-      if (RR)
+      keyPressed[counter] = R;
+      if (R)
         Keyboard.release(keys[counter]);
       else
         Keyboard.press(keys[counter]);
@@ -194,115 +207,116 @@ void loop() {
   else
     counter += 3;
 
+  if (portE == PINE && portE != predStatusE)
+  {
+    predStatusE = portE;
+    MOD = !(PINE & B01000000);
+    if (MOD != keyPressed[12])
+    {
+      keyPressed[12] = MOD;
+      if (MOD)
+      {
+        Serial.print("mod_on\n");
+      }
+      else
+      {
+        Serial.print("mod_off\n");
+        updateEEPROM();
+      }
+    }
+  }
   // SEL - START - R - L - D - U
   //01100000
-  if (portD != PIND || portE != PINE)
-    return;
-
-  bool MOD = !(portE & B01000000);
-  if (MOD != keyPressed[12])
+  if (portD == PIND && portD != predStatusD)
   {
-    keyPressed[12] = MOD;
-    if (MOD)
+    predStatusD = portD;
+    //UP
+    counter++;
+    UP = portD & B00000001;
+    if (UP != keyPressed[counter])
     {
-      Serial.print("mod_on\n");
+      keyPressed[counter] = UP;
+      if (MOD && !UP) {
+        adjustLevel(true);
+      }
+      else
+      {
+        if (UP)
+          Keyboard.release(keys[counter]);
+        else
+          Keyboard.press(keys[counter]);
+      }
     }
-    else
-    {
-      Serial.print("mod_off\n");
-      updateEEPROM();
-    }
-  }
+    counter++;
 
-  //UP
-  counter++;
-  bool UP = portD & B00000001;
-  if (UP != keyPressed[counter])
-  {
-    keyPressed[counter] = UP;
-    if (MOD && !UP) {
-      adjustLevel(true);
-    }
-    else
+    DOWN = portD & B00000010;
+    if (DOWN != keyPressed[counter])
     {
-      if (UP)
+      keyPressed[counter] = DOWN;
+      if (MOD && !DOWN) {
+        adjustLevel(false);
+      }
+      else
+      {
+        if (DOWN)
+          Keyboard.release(keys[counter]);
+        else
+          Keyboard.press(keys[counter]);
+      }
+    }
+    counter++;
+
+    LEFT = portD & B00000100;
+    if (LEFT != keyPressed[counter])
+    {
+      keyPressed[counter] = LEFT;
+      if (LEFT)
+        Keyboard.release(keys[counter]);
+      else
+        Keyboard.press(keys[counter]);
+    }
+    counter++;
+    RIGHT = portD & B00001000;
+    if (RIGHT != keyPressed[counter])
+    {
+      keyPressed[counter] = RIGHT;
+      if (RIGHT)
+        Keyboard.release(keys[counter]);
+      else
+        Keyboard.press(keys[counter]);
+    }
+
+    counter++;
+    START = portD & B10000000;
+    if (START != keyPressed[counter])
+    {
+      keyPressed[counter] = START;
+      if (MOD && !START) {
+        sendCommand(0);
+      }
+      else if (START)
+        Keyboard.release(keys[counter]);
+      else
+        Keyboard.press(keys[counter]);
+    }
+
+    counter++;
+    SELECT = portD & B00010000;
+    if (SELECT != keyPressed[counter])
+    {
+      keyPressed[counter] = SELECT;
+      if (MOD && !SELECT) {
+        //sendCommand(1);
+        Keyboard.press(KEY_ESC);
+        delay(150);
+        Keyboard.release(KEY_ESC);
+      }
+      else if (SELECT)
         Keyboard.release(keys[counter]);
       else
         Keyboard.press(keys[counter]);
     }
   }
-  counter++;
-
-  bool DOWN = portD & B00000010;
-  if (DOWN != keyPressed[counter])
-  {
-    keyPressed[counter] = DOWN;
-    if (MOD && !DOWN) {
-      adjustLevel(false);
-    }
-    else
-    {
-      if (DOWN)
-        Keyboard.release(keys[counter]);
-      else
-        Keyboard.press(keys[counter]);
-    }
-  }
-  counter++;
-
-  bool LEFT = portD & B00000100;
-  if (LEFT != keyPressed[counter])
-  {
-    keyPressed[counter] = LEFT;
-    if (LEFT)
-      Keyboard.release(keys[counter]);
-    else
-      Keyboard.press(keys[counter]);
-  }
-  counter++;
-  bool RIGHT = portD & B00001000;
-  if (RIGHT != keyPressed[counter])
-  {
-    keyPressed[counter] = RIGHT;
-    if (RIGHT)
-      Keyboard.release(keys[counter]);
-    else
-      Keyboard.press(keys[counter]);
-  }
-
-  counter++;
-  bool START = portD & B10000000;
-  if (START != keyPressed[counter])
-  {
-    keyPressed[counter] = START;
-    if (MOD && !START) {
-      sendCommand(0);
-    }
-    else if (START)
-      Keyboard.release(keys[counter]);
-    else
-      Keyboard.press(keys[counter]);
-  }
-
-  counter++;
-  bool SELECT = portD & B00010000;
-  if (SELECT != keyPressed[counter])
-  {
-    keyPressed[counter] = SELECT;
-    if (MOD && !SELECT) {
-      //sendCommand(1);
-      Keyboard.press(KEY_ESC);
-      delay(150);
-      Keyboard.release(KEY_ESC);
-    }
-    else if (SELECT)
-      Keyboard.release(keys[counter]);
-    else
-      Keyboard.press(keys[counter]);
-  }
-
-  if (Serial.available())
-    processSerial();
 }
 
 void processSerial()
@@ -329,18 +343,19 @@ void processSerial()
       PORTB = (PORTB & 0xF1) | ((7 - (level & 7)) << 1);
     }
   }
+  Serial.flush();
 }
 
 void updateEEPROM()
 {
-  EEPROM.update(BrightnessAddress, level);
+  EEPROM.update(BrightnessAddress, max(level, 1));
 }
 
 void adjustLevel(bool up)
 {
   if (up && level < 7)
     level++;
-  else if (!up && level > 0)
+  else if (!up && level > 1)
     level--;
   else
     return;
@@ -361,7 +376,7 @@ void sendCommand(int cmd)
 void readBattery()
 {
   total -= analogread[readIndex];
-  analogread[readIndex] = analogRead(BatteryLevelPin)+OFFSET;
+  analogread[readIndex] = analogRead(BatteryLevelPin) + OFFSET;
   total += analogread[readIndex];
   readIndex++;
   if (readIndex >= WINDOW)
